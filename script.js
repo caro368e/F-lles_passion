8adc4f18713928e60e63673a14d6b26364970e67
iana0030 <iana0030@stud.kea.dk>
2020-02-21 08:55:30 +0100

    javascript

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let alleBokse = [];
+let alleStile = [];
 const endpoint = "https://spreadsheets.google.com/feeds/list/1vOHWbUA6Mg-QQ3r0N3PpWGCtvbjTYtFPFhHHuhAcEUs/od6/public/values?alt=json";
 let filter = "alle";
 
@@ -18,7 +18,6 @@ function burgerClick() {
 }
 
 
-
 document.addEventListener("DOMContentLoaded", start);
 
 function start() {
@@ -31,13 +30,36 @@ function start() {
 async function hentData() {
     console.log("hentData");
     const response = await fetch(endpoint);
-    alleBokse = await response.json();
+    console.log(response);
+    alleStile = await response.json();
+    console.log(alleStile);
     visStil();
 }
 
 //funktion som skal vise boksene med hver japansk stil
 function visStil() {
+    const container = document.querySelector("#data_container");
+    const stilTemplate = document.querySelector("template");
+    container.innerHTML = "";
 
+    alleStile.feed.entry.forEach(stil => {
+        if (filter == "alle" || filter == stil.gsx$navn.$t) {
+            let klon = stilTemplate.cloneNode(true).content;
+
+            klon.querySelector("h3").textContent = stil.gsx$navn.$t;
+            klon.querySelector(".kategori").textContent = stil.gsx$navn.$t;
+            klon.querySelector(".lang").textContent = stil.gsx$navn.$t;
+            klon.querySelector(".udseende").textContent = stil.gsx$navn.$t;
+            klon.querySelector("img").src = "japan_img/imgs/" + stil.gsx$billede.$t + ".jpg";
+            klon.querySelector("img").alt = "billede af " + stil.gsx$navn.$t;
+
+            klon.querySelector(".oversigt").addEventListener("click", () => {
+                location.href = `single.html?id=${stil.gsx$id.$t}`;
+            });
+
+            container.appendChild(klon);
+        }
+    })
 }
 
 
